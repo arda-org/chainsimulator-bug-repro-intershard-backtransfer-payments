79fe1c587ff7094c883e4ff7c7827783692ff4e9
Jad Zahreddine <107253062+jad-zd@users.noreply.github.com>
2024-08-21 09:21:42 +0200

    Add SFT cases

--- contract.test.ts
+++ contract.test.ts
@@ -46,10 +46,43 @@ test("SC A calls SC B, different shards, async v1. SC B sends back FFT", async (
 	});
 
 	assertAccount(await contractB.getAccount(), {
-		kvs: { esdts: [{ id: tokenId, amount: 0 }] },
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 0 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
 	});
 	assertAccount(await contractA.getAccount(), {
 		kvs: { esdts: [{ id: tokenId, amount: 1 }] },
+	});
+});
+
+test("SC A calls SC B, different shards, async v1. SC B sends back SFT", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet.callContract({
+		callee: contractA,
+		funcName: "send_back_from_sc_async_v1",
+		funcArgs: [
+			e.Addr(contractB),
+			e.List(e.Tuple(e.Str(tokenId), e.U64(1), e.U(1))),
+		],
+		gasLimit: 100_000_000,
+	});
+
+	assertAccount(await contractB.getAccount(), {
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 0 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		kvs: { esdts: [{ id: tokenId, nonce: 1, amount: 1 }] },
 	});
 });
 
@@ -78,11 +111,54 @@ test("🔴 SC A calls SC B, different shards, async v1. SC B sends back EGLD+FFT
 
 	assertAccount(await contractB.getAccount(), {
 		balance: 1,
-		kvs: { esdts: [{ id: tokenId, amount: 0 }] },
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 0 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
 	});
 	assertAccount(await contractA.getAccount(), {
 		balance: 0,
 		kvs: { esdts: [{ id: tokenId, amount: 1 }] },
+	});
+});
+
+test("🔴 SC A calls SC B, different shards, async v1. SC B sends back EGLD+SFT", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet
+		.callContract({
+			callee: contractA,
+			funcName: "send_back_from_sc_async_v1",
+			funcArgs: [
+				e.Addr(contractB),
+				e.List(
+					e.Tuple(e.Str("EGLD"), e.U64(0), e.U(1)),
+					e.Tuple(e.Str(tokenId), e.U64(1), e.U(1)),
+				),
+			],
+			gasLimit: 100_000_000,
+		})
+		.assertFail({
+			code: "returnMessage",
+			message: "sending value to non payable contract",
+		});
+
+	assertAccount(await contractB.getAccount(), {
+		balance: 1,
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 0 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		balance: 0,
+		kvs: { esdts: [{ id: tokenId, nonce: 1, amount: 1 }] },
 	});
 });
 
@@ -111,11 +187,126 @@ test("🔴 SC A calls SC B, different shards, async v1. SC B sends back FFT+EGLD
 
 	assertAccount(await contractB.getAccount(), {
 		balance: 0,
-		kvs: { esdts: [{ id: tokenId, amount: 1 }] },
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
 	});
 	assertAccount(await contractA.getAccount(), {
 		balance: 1,
 		kvs: { esdts: [{ id: tokenId, amount: 0 }] },
+	});
+});
+
+test("🔴 SC A calls SC B, different shards, async v1. SC B sends back FFT+SFT", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet
+		.callContract({
+			callee: contractA,
+			funcName: "send_back_from_sc_async_v1",
+			funcArgs: [
+				e.Addr(contractB),
+				e.List(
+					e.Tuple(e.Str(tokenId), e.U64(0), e.U(1)),
+					e.Tuple(e.Str(tokenId), e.U64(1), e.U(1)),
+				),
+			],
+			gasLimit: 100_000_000,
+		})
+		.assertFail({
+			code: "returnMessage",
+			message: "sending value to non payable contract",
+		});
+
+	assertAccount(await contractB.getAccount(), {
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 0 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		kvs: { esdts: [{ id: tokenId, nonce: 1, amount: 1 }] },
+	});
+});
+
+test("🔴 SC A calls SC B, different shards, async v1. SC B sends back SFT+EGLD", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet
+		.callContract({
+			callee: contractA,
+			funcName: "send_back_from_sc_async_v1",
+			funcArgs: [
+				e.Addr(contractB),
+				e.List(
+					e.Tuple(e.Str(tokenId), e.U64(1), e.U(1)),
+					e.Tuple(e.Str("EGLD"), e.U64(0), e.U(1)),
+				),
+			],
+			gasLimit: 100_000_000,
+		})
+		.assertFail({
+			code: "returnMessage",
+			message: "sending value to non payable contract",
+		});
+
+	assertAccount(await contractB.getAccount(), {
+		balance: 0,
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		balance: 1,
+		kvs: { esdts: [{ id: tokenId, nonce: 1, amount: 0 }] },
+	});
+});
+
+test("🔴 SC A calls SC B, different shards, async v1. SC B sends back SFT+FFT", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet
+		.callContract({
+			callee: contractA,
+			funcName: "send_back_from_sc_async_v1",
+			funcArgs: [
+				e.Addr(contractB),
+				e.List(
+					e.Tuple(e.Str(tokenId), e.U64(1), e.U(1)),
+					e.Tuple(e.Str(tokenId), e.U64(0), e.U(1)),
+				),
+			],
+			gasLimit: 100_000_000,
+		})
+		.assertFail({
+			code: "returnMessage",
+			message: "sending value to non payable contract",
+		});
+
+	assertAccount(await contractB.getAccount(), {
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 0 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		kvs: { esdts: [{ id: tokenId, amount: 1 }] },
 	});
 });
 
@@ -160,10 +351,44 @@ test("SC A calls SC B, different shards, async v2. SC B sends back FFT", async (
 	});
 
 	assertAccount(await contractB.getAccount(), {
-		kvs: { esdts: [{ id: tokenId, amount: 0 }] },
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 0 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
 	});
 	assertAccount(await contractA.getAccount(), {
 		kvs: { esdts: [{ id: tokenId, amount: 1 }] },
+	});
+});
+
+test("SC A calls SC B, different shards, async v2. SC B sends back SFT", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet.callContract({
+		callee: contractA,
+		funcName: "send_back_from_sc_async_v2",
+		funcArgs: [
+			e.Addr(contractB),
+			e.List(e.Tuple(e.Str(tokenId), e.U64(1), e.U(1))),
+			e.U64(10_000_000),
+		],
+		gasLimit: 100_000_000,
+	});
+
+	assertAccount(await contractB.getAccount(), {
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 0 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		kvs: { esdts: [{ id: tokenId, nonce: 1, amount: 1 }] },
 	});
 });
 
@@ -193,11 +418,55 @@ test("🔴 SC A calls SC B, different shards, async v2. SC B sends back EGLD+FFT
 
 	assertAccount(await contractB.getAccount(), {
 		balance: 1,
-		kvs: { esdts: [{ id: tokenId, amount: 0 }] },
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 0 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
 	});
 	assertAccount(await contractA.getAccount(), {
 		balance: 0,
 		kvs: { esdts: [{ id: tokenId, amount: 1 }] },
+	});
+});
+
+test("🔴 SC A calls SC B, different shards, async v2. SC B sends back EGLD+SFT", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet
+		.callContract({
+			callee: contractA,
+			funcName: "send_back_from_sc_async_v2",
+			funcArgs: [
+				e.Addr(contractB),
+				e.List(
+					e.Tuple(e.Str("EGLD"), e.U64(0), e.U(1)),
+					e.Tuple(e.Str(tokenId), e.U64(1), e.U(1)),
+				),
+				e.U64(10_000_000),
+			],
+			gasLimit: 100_000_000,
+		})
+		.assertFail({
+			code: "returnMessage",
+			message: "sending value to non payable contract",
+		});
+
+	assertAccount(await contractB.getAccount(), {
+		balance: 1,
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 0 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		balance: 0,
+		kvs: { esdts: [{ id: tokenId, nonce: 1, amount: 1 }] },
 	});
 });
 
@@ -227,11 +496,128 @@ test("🔴 SC A calls SC B, different shards, async v2. SC B sends back FFT+EGLD
 
 	assertAccount(await contractB.getAccount(), {
 		balance: 0,
-		kvs: { esdts: [{ id: tokenId, amount: 1 }] },
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
 	});
 	assertAccount(await contractA.getAccount(), {
 		balance: 1,
 		kvs: { esdts: [{ id: tokenId, amount: 0 }] },
+	});
+});
+test("🔴 SC A calls SC B, different shards, async v2. SC B sends back FFT+SFT", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet
+		.callContract({
+			callee: contractA,
+			funcName: "send_back_from_sc_async_v2",
+			funcArgs: [
+				e.Addr(contractB),
+				e.List(
+					e.Tuple(e.Str(tokenId), e.U64(0), e.U(1)),
+					e.Tuple(e.Str(tokenId), e.U64(1), e.U(1)),
+				),
+				e.U64(10_000_000),
+			],
+			gasLimit: 100_000_000,
+		})
+		.assertFail({
+			code: "returnMessage",
+			message: "sending value to non payable contract",
+		});
+
+	assertAccount(await contractB.getAccount(), {
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 0 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		kvs: { esdts: [{ id: tokenId, nonce: 1, amount: 1 }] },
+	});
+});
+
+test("🔴 SC A calls SC B, different shards, async v2. SC B sends back SFT+EGLD", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet
+		.callContract({
+			callee: contractA,
+			funcName: "send_back_from_sc_async_v1",
+			funcArgs: [
+				e.Addr(contractB),
+				e.List(
+					e.Tuple(e.Str(tokenId), e.U64(1), e.U(1)),
+					e.Tuple(e.Str("EGLD"), e.U64(0), e.U(1)),
+				),
+				e.U64(10_000_000),
+			],
+			gasLimit: 100_000_000,
+		})
+		.assertFail({
+			code: "returnMessage",
+			message: "sending value to non payable contract",
+		});
+
+	assertAccount(await contractB.getAccount(), {
+		balance: 0,
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		balance: 1,
+		kvs: { esdts: [{ id: tokenId, nonce: 1, amount: 0 }] },
+	});
+});
+
+test("🔴 SC A calls SC B, different shards, async v2. SC B sends back SFT+FFT", async () => {
+	using world = await FSWorld.start();
+
+	const { wallet, contractA, contractB } = await createAccounts(world);
+
+	await wallet
+		.callContract({
+			callee: contractA,
+			funcName: "send_back_from_sc_async_v2",
+			funcArgs: [
+				e.Addr(contractB),
+				e.List(
+					e.Tuple(e.Str(tokenId), e.U64(1), e.U(1)),
+					e.Tuple(e.Str(tokenId), e.U64(0), e.U(1)),
+				),
+				e.U64(10_000_000),
+			],
+			gasLimit: 100_000_000,
+		})
+		.assertFail({
+			code: "returnMessage",
+			message: "sending value to non payable contract",
+		});
+
+	assertAccount(await contractB.getAccount(), {
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 0 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
+	});
+	assertAccount(await contractA.getAccount(), {
+		kvs: { esdts: [{ id: tokenId, amount: 1 }] },
 	});
 });
 
@@ -259,7 +645,18 @@ const createAccounts = async (
 		code: wasmCodePath,
 		codeMetadata: [],
 		balance: 1,
-		kvs: { esdts: [{ id: tokenId, amount: 1 }] },
+		kvs: {
+			esdts: [
+				{ id: tokenId, amount: 1 },
+				{ id: tokenId, nonce: 1, amount: 1 },
+			],
+		},
+	});
+	await world.setAccount({
+		address: "erd1lllllllllllllllllllllllllllllllllllllllllllllllllllsckry7t",
+		kvs: {
+			esdts: [{ id: tokenId, nonce: 1, amount: 1 }],
+		},
 	});
 	return { wallet, contractA, contractB };
 };
